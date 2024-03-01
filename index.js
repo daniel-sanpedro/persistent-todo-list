88c5aaa3ae6092002e434937e9e71807587426c4
daniel-sanpedro <dpsanpedro@yahoo.com>
2024-02-29 23:50:47 -0800

    < added html and js>

---
+++ index.js
@@ -0,0 +1,25 @@
+let items = [];
+
+const itemsDiv = document.getElementById("items");
+// load items
+function loadItems() {}
+
+// render items
+function renderitems() {
+  // this clears the div and rewrites it
+  itemsDiv.innerHTML = null;
+  // [inx, itm]-
+  for (const [idx, item] of Object.entries(items)) {
+
+    
+  }
+}
+
+// save items
+function saveItems() {}
+
+// add item
+function addItem() {}
+
+// remove item
+function removeItem() {}
